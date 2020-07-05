6718ec8a8382901929d3b68580cc7226824ad1f2
PRANAVAN12 <npranavan28@gmail.com>
2020-07-04 19:06:25 -0700

    crud Application C#

---
+++ Crud-MVC/Controllers/eMPController.cs
@@ -0,0 +1,25 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Crud_MVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Crud_MVC.Controllers
+{
+    public class eMPController : Controller
+    {
+        private readonly AppilicationDbContext _db;
+       public eMPController(AppilicationDbContext  db)
+        {
+            _db = db;
+
+        }
+        public IActionResult Index()
+        {
+            var displaydata = _db.EmployeeTable.ToList();
+            return View();
+        }
+    }
+}
---
+++ Crud-MVC/Models/AppilicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Crud_MVC.Models
+{
+    public class AppilicationDbContext : DbContext
+    {
+        public AppilicationDbContext(DbContextOptions<AppilicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<NewEmpClass> EmployeeTable { get; set;  }
+    }
+}
